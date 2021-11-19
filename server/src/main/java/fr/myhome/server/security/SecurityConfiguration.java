20125ea58b3f3b525bda6fb5dee6baed17419832
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-11-19 16:18:56 +0100

    update cors configuration

--- server/src/main/java/fr/myhome/server/security/SecurityConfiguration.java
+++ server/src/main/java/fr/myhome/server/security/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package fr.myhome.server.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +31,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated();
 
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(@NonNull final CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins("http://localhost:9200", "http://localhost:3000")
+                    .allowCredentials(true);
+            }
+        };
+    }
+
     
 }
