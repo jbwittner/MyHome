411d857ed60d25f9e95e25b72184dae0beb7fe8b
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-11-23 00:43:16 +0100

    Adding PMD

--- server/src/main/java/fr/myhome/server/MyHomeServerApplication.java
+++ server/src/main/java/fr/myhome/server/MyHomeServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyHomeServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(MyHomeServerApplication.class, args);
 	}
 
