391a0410ead4666321a6e22fb9da09457fb4568c
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-11-23 01:36:47 +0100

    update repository querry

--- server/src/main/java/fr/myhome/server/repository/UserRepository.java
+++ server/src/main/java/fr/myhome/server/repository/UserRepository.java
@@ -6,6 +6,6 @@ import fr.myhome.server.model.User;
 
 public interface UserRepository extends AbstractRepository<User, Integer>{
     
-    Optional<User> findByUserName(String userName);
+    Optional<User> findByUsername(String username);
 
 }
