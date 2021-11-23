02d7dea7eb6a1dc7c593f912ed68226fee58d40e
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-11-23 13:32:06 +0100

    Add exceptions

---
+++ server/src/main/java/fr/myhome/server/exception/FunctionalException.java
@@ -0,0 +1,11 @@
+package fr.myhome.server.exception;
+
+public abstract class FunctionalException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FunctionalException(final String message){
+        super(message);
+    }
+    
+}
