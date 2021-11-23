0e3df3dfb30c45e89b9c6777bd0c0f0d37b501c9
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-11-23 01:27:18 +0100

    Adding generated part

---
+++ server/src/main/java/fr/myhome/server/generated/model/UserDTO.java
@@ -0,0 +1,194 @@
+package fr.myhome.server.generated.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import fr.myhome.server.generated.model.UserRoleEnum;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * UserDTO
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class UserDTO   {
+  @JsonProperty("userName")
+  private String userName;
+
+  @JsonProperty("email")
+  private String email;
+
+  @JsonProperty("firstName")
+  private String firstName;
+
+  @JsonProperty("lastName")
+  private String lastName;
+
+  @JsonProperty("roles")
+  @Valid
+  private List<UserRoleEnum> roles = new ArrayList<UserRoleEnum>();
+
+  public UserDTO userName(String userName) {
+    this.userName = userName;
+    return this;
+  }
+
+  /**
+   * Get userName
+   * @return userName
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public UserDTO email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  /**
+   * Get email
+   * @return email
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public UserDTO firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * Get firstName
+   * @return firstName
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public UserDTO lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * Get lastName
+   * @return lastName
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public UserDTO roles(List<UserRoleEnum> roles) {
+    this.roles = roles;
+    return this;
+  }
+
+  public UserDTO addRolesItem(UserRoleEnum rolesItem) {
+    this.roles.add(rolesItem);
+    return this;
+  }
+
+  /**
+   * Get roles
+   * @return roles
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+  @Valid
+
+  public List<UserRoleEnum> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<UserRoleEnum> roles) {
+    this.roles = roles;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UserDTO userDTO = (UserDTO) o;
+    return Objects.equals(this.userName, userDTO.userName) &&
+        Objects.equals(this.email, userDTO.email) &&
+        Objects.equals(this.firstName, userDTO.firstName) &&
+        Objects.equals(this.lastName, userDTO.lastName) &&
+        Objects.equals(this.roles, userDTO.roles);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(userName, email, firstName, lastName, roles);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UserDTO {\n");
+    
+    sb.append("    userName: ").append(toIndentedString(userName)).append("\n");
+    sb.append("    email: ").append(toIndentedString(email)).append("\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("    roles: ").append(toIndentedString(roles)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ server/src/main/java/fr/myhome/server/generated/model/UserRoleEnum.java
@@ -0,0 +1,46 @@
+package fr.myhome.server.generated.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+/**
+ * Gets or Sets UserRoleEnum
+ */
+public enum UserRoleEnum {
+  
+  ADMIN("ADMIN"),
+  
+  USER("USER");
+
+  private String value;
+
+  UserRoleEnum(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static UserRoleEnum fromValue(String value) {
+    for (UserRoleEnum b : UserRoleEnum.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
+}
+
