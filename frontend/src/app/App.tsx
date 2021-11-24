2ec757e590924ec5915da940f8245686951c0c62
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-11-25 00:04:09 +0100

    Update root part of frontend

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,29 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import React, { useState } from 'react';
+import { LoginContext } from './context/Context';
+import { MainRouter } from './router/Router';
+import { AppTheme } from './theme/Theme';
+
+function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ThemeProvider theme={AppTheme}>
+                    <CssBaseline />
+                        <LoginContext.Provider
+                            value={{
+                                isAuthenticated,
+                                setIsAuthenticated
+                            }}
+                        >
+                            <LoginContext.Consumer>{() => <MainRouter />}</LoginContext.Consumer>
+                        </LoginContext.Provider>
+                </ThemeProvider>
+            </header>
+        </div>
+    );
+}
+
+export default App;
